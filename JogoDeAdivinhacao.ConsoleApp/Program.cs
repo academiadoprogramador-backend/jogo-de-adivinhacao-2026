10eb3348b34d0a37f10e0577611ca79c5f4fbfdf
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-03-19 20:51:00 -0300

    Adicionadas múltiplas tentativas

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -12,28 +12,42 @@
         O sistema informará o usuário se o mesmo acertou ou não, podendo incluir dicas sobre a proximidade do "chute"
 */
 
-// 3. Nosso jogo deve validar a tentativa do jogador e exibir uma mensagem de feedback
-Console.WriteLine("-----------------------------------------------");
-Console.WriteLine("Jogo de Adivinhação");
-Console.WriteLine("-----------------------------------------------");
-
+// 4. Nosso jogo deve permitir múltiplas tentativas de adivinhação
 int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21);
 
-Console.WriteLine();
-Console.Write("Digite um número: ");
-int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+bool jogoDeveContinuar = true;
 
-if (numeroDigitado == numeroAleatorio)
+while (jogoDeveContinuar == true) // false
 {
-    Console.WriteLine("Parabéns, você acertou! O número era " + numeroAleatorio);
-}
-else if (numeroDigitado > numeroAleatorio)
-{
-    Console.WriteLine("O número digitado foi maior que o número secreto!");
-}
-else
-{
-    Console.WriteLine("O número digitado foi menor que o número secreto!");
+    Console.Clear();
+    Console.WriteLine("-----------------------------------------------");
+    Console.WriteLine("Jogo de Adivinhação");
+    Console.WriteLine("-----------------------------------------------");
+
+    Console.WriteLine();
+    Console.Write("Digite um número: ");
+    int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+    if (numeroDigitado == numeroAleatorio)
+    {
+        Console.WriteLine("Parabéns, você acertou! O número era " + numeroAleatorio);
+    }
+    else if (numeroDigitado > numeroAleatorio)
+    {
+        Console.WriteLine("O número digitado foi maior que o número secreto!");
+    }
+    else
+    {
+        Console.WriteLine("O número digitado foi menor que o número secreto!");
+    }
+
+    Console.WriteLine();
+    Console.Write("Deseja continuar? (s/N): ");
+    string opcaoContinuar = Console.ReadLine();
+
+    if (opcaoContinuar != "S" && opcaoContinuar != "s")
+    {
+        jogoDeveContinuar = false;
+    }
 }
 
-Console.ReadLine();
