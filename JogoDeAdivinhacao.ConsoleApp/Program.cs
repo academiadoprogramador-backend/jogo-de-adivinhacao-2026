566ccffc6c3e5796e0b504cd1811191fc11d1964
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-03-19 20:31:27 -0300

    Adicionada comparação do número digitado vs número aleatório

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -12,18 +12,28 @@
         O sistema informará o usuário se o mesmo acertou ou não, podendo incluir dicas sobre a proximidade do "chute"
 */
 
-// 2. Nosso jogo deve gerar um número secreto aleatório
+// 3. Nosso jogo deve validar a tentativa do jogador e exibir uma mensagem de feedback
 Console.WriteLine("-----------------------------------------------");
 Console.WriteLine("Jogo de Adivinhação");
 Console.WriteLine("-----------------------------------------------");
 
-Console.WriteLine();
-Console.Write("Digite um número: ");
-string strNumeroDigitado = Console.ReadLine();
-
-// 1 - 20 (numero minimo, numero maximo (exclusivo))
 int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21);
 
-Console.WriteLine("O número aleatório foi: " + numeroAleatorio);
+Console.WriteLine();
+Console.Write("Digite um número: ");
+int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+if (numeroDigitado == numeroAleatorio)
+{
+    Console.WriteLine("Parabéns, você acertou! O número era " + numeroAleatorio);
+}
+else if (numeroDigitado > numeroAleatorio)
+{
+    Console.WriteLine("O número digitado foi maior que o número secreto!");
+}
+else
+{
+    Console.WriteLine("O número digitado foi menor que o número secreto!");
+}
 
 Console.ReadLine();
